b952aebdb8e45a866c80f87c036d40c74cf5f8e2
Tomasz Magulski <tomasz@magullab.io>
2016-12-12 00:37:39 +0100

    Opening pull requests

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8  -*-
 
+import credentials
 from pwbdb import gerrit, github
 
 if __name__ == '__main__':
     for change in gerrit.get_changes():
+        print(change['_number'])
         local_branch = gerrit.fetch_branch(change)
-        github.push_branch(local_branch)
+        github_ref = github.push_branch(local_branch)
+        github.open_pull_request(github_ref, creds=credentials)
--- pwbdb/github.py
+++ pwbdb/github.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8  -*-
 
+from github import Github
+
+
+GITHUB_REPO = 'magul/pywikibot-core'
+
 
 def push_branch(local_branch):
     name = local_branch.name
     repo = local_branch.repo
     github = repo.remotes['github']
 
-    if 'github/{}'.format(name) not in github.fetch():
-        github.push(name)
+    return github.push(name)[0].remote_ref
+
+
+def open_pull_request(branch, creds=None):
+    if creds is None:
+        creds = {}
+
+    github = Github(creds.GITHUB_USER, creds.GITHUB_PASS)
+
+    name = branch.remote_head
+
+    repo = github.get_repo(GITHUB_REPO)
+
+    pull_requests = [p for p in repo.get_pulls() if p.head.ref == name]
+    if len(pull_requests) > 0:
+        return pull_requests[0]
+    return repo.create_pull(title=name, body='', head=name, base='master')
